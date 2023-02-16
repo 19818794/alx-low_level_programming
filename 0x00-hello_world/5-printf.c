bb3c6b60d68cf28cd42563f7bbb3573df7b80d75
19818794 <mona2afanga@gmail.com>
2023-02-16 12:37:34 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program prints a line using puts function'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+/* print a line using puts function */
+printf("with proper grammar, but the outcome is a piece of art,");
+/* return 0 as a success operation */
+return (0);
+}
