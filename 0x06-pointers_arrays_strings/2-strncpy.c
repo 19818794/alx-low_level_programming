8728ad939501fd67313c1082592aadb9bba9237d
19818794 <mona2afanga@gmail.com>
2023-03-01 12:49:24 -0800

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -15,8 +15,11 @@ char *_strncpy(char *dest, char *src, int n)
 	{
 		dest[i] = src[i];
 		i++;
-		if (n >= 90)
-			dest[i] = '\0';
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
 	}
 	return (dest);
 }
