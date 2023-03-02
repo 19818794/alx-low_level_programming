22cf8df12c721ebf808fb571773f375ec9fcea1b
19818794 <mona2afanga@gmail.com>
2023-03-02 03:34:37 -0800

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include "main.h"
 /**
  * *cap_string - capitalizes all words of a string.
@@ -8,24 +9,44 @@
 char *cap_string(char *str)
 {
 	char *ptr = str;
+	bool cond;
 
 	while (*ptr != '\0')
 	{
-		if (*ptr == 32 || *ptr == 9 || *ptr == 10 || *ptr == 44 ||
+		cond = (*ptr == 32 || *ptr == 9 || *ptr == 10 || *ptr == 44 ||
 		*ptr == 59 || *ptr == 46 || *ptr == 33 || *ptr == 63 ||
 		*ptr == 34 || *ptr == 40 || *ptr == 41 || *ptr == 123 ||
-		*ptr == 125)
+		*ptr == 125);
+		if (cond)
 		{
 			ptr++;
-			if (*ptr == 32 || *ptr == 9 || *ptr == 10 || *ptr == 44
-			|| *ptr == 59 || *ptr == 46 || *ptr == 33 || *ptr == 63
-			|| *ptr == 34 || *ptr == 40 || *ptr == 41 ||
-			*ptr == 123 || *ptr == 125)
+			cond = (*ptr == 32 || *ptr == 9 || *ptr == 10 ||
+			*ptr == 44 || *ptr == 59 || *ptr == 46 || *ptr == 33 ||
+			*ptr == 63 || *ptr == 34 || *ptr == 40 || *ptr == 41 ||
+			*ptr == 123 || *ptr == 125);
+			if (cond)
+			{
 				ptr++;
+				cond = (*ptr == 32 || *ptr == 9 || *ptr == 10 ||
+				*ptr == 44 || *ptr == 59 || *ptr == 46 ||
+				*ptr == 33 || *ptr == 63 || *ptr == 34 ||
+				*ptr == 40 || *ptr == 41 || *ptr == 123 ||
+				*ptr == 125);
+				if (cond)
+				{
+					ptr++;
+				}
+			}
 			if (*ptr >= 97 && *ptr <= 122)
+			{
 				*ptr = *ptr - 32;
+				ptr++;
+			}
 		}
-		ptr++;
+		else
+		{
+			ptr++;
+		}
 	}
 	return (str);
 }
