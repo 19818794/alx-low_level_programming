f75a23ddb69dae7a05e0a3b8bf75cb3990a15a09
19818794 <mona2afanga@gmail.com>
2023-03-08 06:58:07 -0800

    Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,6 @@
+#include <stdio.h>
 #include "main.h"
-int _sqrt_rec(int m, int n);
+int _sqrt_rec(long int m, long int n);
 /**
  * _sqrt_recursion - returns the natural square root of a number.
  * @n: a number
@@ -9,7 +10,10 @@ int _sqrt_rec(int m, int n);
  */
 int _sqrt_recursion(int n)
 {
-	return (_sqrt_rec(1, n));
+	long int k;
+
+	k = (long int)n;
+	return (_sqrt_rec(1, k));
 }
 
 /**
@@ -19,20 +23,28 @@ int _sqrt_recursion(int n)
  * Return: If n does not have a natural square root, the function returns -1
  * otherwise returns the natural square root of n
  */
-int _sqrt_rec(int m, int n)
+int _sqrt_rec(long int m, long int n)
 {
-	int next_m;
+	long int next_m;
 
 	next_m = (m + (n / m)) / 2;
 	if (m > n)
-		return (-1);
-	if (m * m <= n && (m + 1) * (m + 1) > n)
 	{
-		if (m * m == n)
+		return (-1);
+	}
+	else if (((m * m) <= n) && (((m + 1) * (m + 1)) > n))
+	{
+		if ((m * m) == n)
+		{
 			return (m);
+		}
 		else
+		{
 			return (-1);
+		}
 	}
 	else
+	{
 		return (_sqrt_rec(next_m, n));
+	}
 }
