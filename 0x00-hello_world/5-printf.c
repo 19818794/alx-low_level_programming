759c64bde4a176ebe6933eedd1bc68b5de3f887d
19818794 <mona2afanga@gmail.com>
2023-03-18 04:56:29 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program prints a line using printf function'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+/* print a line using printf function */
+printf("with proper grammar, but the outcome is a piece of art,\n");
+/* return 0 as a success operation */
+return (0);
+}
