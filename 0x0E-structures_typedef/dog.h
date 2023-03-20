a516920e46669f8c70e553475f7437128c3895a7
19818794 <mona2afanga@gmail.com>
2023-03-20 14:24:53 -0700

    Poppy Updated

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - type struct dog
+ * @name: the dog's name
+ * @age: the dog's age
+ * @owner: the dog's owner
+ *
+ * Description: a type for dog animals.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
