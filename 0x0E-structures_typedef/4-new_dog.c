c4de3fa58a18ece3ea14e22a3ebbb118f6f8bf35
19818794 <mona2afanga@gmail.com>
2023-03-20 14:31:10 -0700

    A door is what a dog is perpetually on the wrong side of

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,75 @@
+#include <stdlib.h>
+#include "dog.h"
+
+int _strlen(char *str);
+char *_strcopy(char *dest, char *src);
+
+/**
+ * new_dog - creates a new dog.
+ * @name: the dog's name.
+ * @age: the dof's age.
+ * @owner: the dog's owner.
+ *
+ * Return: a pointer of dog_t
+ * otherwise NULL if fails.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+
+	if (name == NULL || age < 0 || owner == NULL)
+		return (NULL);
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
+		return (NULL);
+	d->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if (d->name == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+	d->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (d->owner == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+
+	d->name = _strcopy(d->name, name);
+	d->age = age;
+	d->owner = _strcopy(d->owner, owner);
+
+	return (d);
+}
+
+/**
+ * _strlen - checks the lengh of a string.
+ * @str: the string will be checked.
+ *
+ * Return: string's lengh
+ */
+int _strlen(char *str)
+{
+	int len = 0;
+
+	while (*str++)
+		len++;
+	return (len);
+}
+
+/**
+ * _strcopy - copies a source string to a destination string.
+ * @dest: the destination string.
+ * @src: the source string.
+ *
+ * Return: destination string.
+ */
+char *_strcopy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i]; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
